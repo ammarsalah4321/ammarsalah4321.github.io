68eaa01556f5263372990874a030d04c9e730a75
Ammar <ammar.salah62012>
2025-05-24 18:19:03 +0300

    making the dist

---
+++ dist/assets/Header_image.jsx
@@ -0,0 +1,7 @@
+import header_img from './imgs/Header_image.jpg'
+
+export default function HeaderImage() {
+    return (
+        <img src={header_img} alt="header image" className='p-3 flex max-w-full max-h-full' />
+    )
+}
