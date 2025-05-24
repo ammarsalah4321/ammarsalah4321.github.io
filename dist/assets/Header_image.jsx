8289c3fdc75e4f269b8e40e8186141789a7380d4
Ammar <ammar.salah62012>
2025-05-24 19:48:43 +0300

    pushing online

--- dist/assets/Header_image.jsx
+++
@@ -1,7 +0,0 @@
-import header_img from './imgs/Header_image.jpg'
-
-export default function HeaderImage() {
-    return (
-        <img src={header_img} alt="header image" className='p-3 flex max-w-full max-h-full' />
-    )
-}
