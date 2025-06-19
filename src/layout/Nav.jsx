23173f2c27072846bb420a49dbd5d29a5acecde3
Ammar Salah <ammar.salah62012@gmail.com>
2025-06-19 23:01:45 +0300

    starting to focus on the nav for routing

--- src/layout/Nav.jsx
+++ src/layout/Nav.jsx
@@ -9,7 +9,7 @@ export default function Nav() {
                 <div className=" flex items-center justify-end">
                     <button className="rounded bg-cyan-700 font-normal text-blue-950 m-1 p-1.5 cursor-pointer border-none hover:text-shadow-lg text-shadow-black">Login</button>
                     {/**<button className="text-blue-600 font-normal text-2xl rounded bg-cyan-800 p-1.5 m-1 cursor-pointer hover:text-shadow-lg text-shadow-black">login</button>*/}
-                    
+
                     <button className="rounded bg-red-700 font-normal text-red-950 m-1 p-1.5 cursor-pointer border-none hover:text-shadow-lg text-shadow-black">Logout</button>
                     {/**<button className="text-red-600 font-normal text-2xl rounded bg-red-800 p-1.5 m-1 cursor-pointer hover:text-shadow-lg text-shadow-black">logout</button>*/}
 
@@ -17,8 +17,13 @@ export default function Nav() {
 
             </div>
 
-            <div className="flex justify-center items-center font-bold flex-wrap max-w-full text-blue-700 cursor-pointer">
-                links
+            <div className="border-2 border-blue-800 flex justify-center items-center font-bold flex-wrap text-blue-700 cursor-pointer">
+                
+                    <a href="">link2</a>
+                    <a href="">link1</a>
+                    <a href="">link3</a>
+                    <a href="">link4</a>
+
             </div>
         </nav>
     )
